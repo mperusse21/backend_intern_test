8fd31e33d9e62091c7f45a406bd5e5677844d83d
mperusse <mperusse@protonmail.com>
2025-02-16 14:46:28 -0500

    Add test for createSomething

--- test/something.test.ts
+++ test/something.test.ts
@@ -1,16 +1,46 @@
 import { expect } from "chai";
 import request from "supertest";
 import app from "../src/app";
+import { prisma } from "../src/graphql/schema";
+import sinon from "sinon";
 
 describe("Something type api tests", () => {
+  // Reset all stubs after tests finish.
+  after(() => {
+    sinon.restore();
+  });
+
   it("Should respond with hello world status 200", async () => {
     const response = await request(app)
       .post("/graphql")
       .send({ query: "query { hello }" });
 
     expect(response.status).to.be.equal(200);
-    expect(response.body.data).to.deep.equal({
-      hello: "world",
+    expect(response.body.data).to.deep.equal({ hello: "world" });
+  });
+
+  it("Should respond with created Something and status 200", async () => {
+    const createSomethingResult = {
+      id: "1234",
+      name: "test",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    // When I tried using 'prismaCreateStub = sinon.stub(prisma.something, "create")' it was still adding to the db.
+    // Found I needed to stub it directly for it to work:
+    // https://medium.com/@wendryo.sales/efficient-mocking-of-prismaclient-a-step-by-step-guide-to-mocking-the-prismaclient-for-effective-997d49a9208f
+    prisma.something.create = sinon
+      .stub()
+      .withArgs({ input: { name: "test" } })
+      .resolves(createSomethingResult);
+
+    const response = await request(app).post("/graphql").send({
+      query: `mutation { createSomething(input: {name: "test"}) { name id } }`,
     });
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body.data.createSomething.id).to.equal("1234");
+    expect(response.body.data.createSomething.name).to.equal("test");
   });
 });
