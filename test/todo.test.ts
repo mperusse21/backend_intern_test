892e10d9ea8a7485f6dcea35f243adf2cc390aa8
mperusse <mperusse@protonmail.com>
2025-02-16 19:18:22 -0500

    Move find todo stubs to before

--- test/todo.test.ts
+++ test/todo.test.ts
@@ -5,36 +5,8 @@ import { prisma } from "../src/graphql/schema";
 import sinon from "sinon";
 
 describe("Todo API query tests", () => {
-  // Reset all stubs after tests finish.
-  after(() => {
-    sinon.restore();
-  });
-
-  it("Should respond with unique Todo and status 200", async () => {
-    const findUniqueResult = {
-      id: "1234",
-      title: "test",
-      completed: false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      dueDate: null,
-    };
-
-    prisma.todo.findUnique = sinon
-      .stub()
-      .withArgs({ id: "1234" })
-      .resolves(findUniqueResult);
-
-    const response = await request(app)
-      .post("/graphql")
-      .send({ query: `query { todo(id: "1234"){ id title }}` });
-
-    expect(response.status).to.be.equal(200);
-    expect(response.body.data.todo.id).to.equal("1234");
-    expect(response.body.data.todo.title).to.equal("test");
-  });
-
-  it("Should respond with array of Todos and status 200", async () => {
+  // Create the stubs before starting the tests
+  before(() => {
     const findManyResult = [
       {
         id: "1",
@@ -54,8 +26,38 @@ describe("Todo API query tests", () => {
       },
     ];
 
-    prisma.todo.findMany = sinon.stub().resolves(findManyResult);
+    const findUniqueResult = {
+      id: "1234",
+      title: "test",
+      completed: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      dueDate: null,
+    };
 
+    prisma.todo.findUnique = sinon
+      .stub()
+      .withArgs({ id: "1234" })
+      .resolves(findUniqueResult);
+    prisma.todo.findMany = sinon.stub().resolves(findManyResult);
+  });
+
+  // Reset all stubs after tests finish.
+  after(() => {
+    sinon.restore();
+  });
+
+  it("Should respond with unique Todo and status 200", async () => {
+    const response = await request(app)
+      .post("/graphql")
+      .send({ query: `query { todo(id: "1234"){ id title }}` });
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body.data.todo.id).to.equal("1234");
+    expect(response.body.data.todo.title).to.equal("test");
+  });
+
+  it("Should respond with array of Todos and status 200", async () => {
     const response = await request(app).post("/graphql").send({
       query: `query { todos{ id title completed createdAt updatedAt dueDate }}`,
     });
